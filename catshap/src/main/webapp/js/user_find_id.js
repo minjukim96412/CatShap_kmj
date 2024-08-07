409707d254c82fcfef10269366dad571fad4172c
yunju <dbswnfnal@naver.com>
2024-08-07 12:30:22 +0900

    fix : 유효성 검사 함수 호출하여 코드 분리

--- catshap/src/main/webapp/js/user_find_id.js
+++ catshap/src/main/webapp/js/user_find_id.js
@@ -30,33 +30,9 @@ $(function () {
 
 // 유효성 검사 함수
 const validate = () => {
-    const unameValid = validateField('#uname', '#uname-error', '이름을 입력해주세요.');
+    const unameValid = validateField('#uname', '#uname-error', '이름을 입력해주세요.') &&
+    	validateFormat('#uname', '#uname-error', '이름 형식이 올바르지 않습니다.', regex.uname);
     const emailValid = validateField('#email', '#email-error', '이메일을 입력해주세요.') &&
-        validateEmailFormat('#email', '#email-error', '이메일 형식이 올바르지 않습니다.');
+        validateFormat('#email', '#email-error', '이메일 형식이 올바르지 않습니다.', regex.email);
     return unameValid && emailValid;
-}
-
-// 이름, 이메일 입력했는지 확인 함수
-const validateField = (selector, errorSelector, errorMessage) => {
-    const value = $(selector).val().trim();
-    if (value === '') {
-        $(errorSelector).text(errorMessage).show();
-        return false;
-    } else {
-        $(errorSelector).hide();
-        return true;
-    }
-} 
-
-// 이메일 형식 확인 함수
-const validateEmailFormat = (selector, errorSelector, errorMessage) => {
-    const value = $(selector).val().trim();
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; // 기본 이메일 정규 표현식
-    if (!emailPattern.test(value)) {
-        $(errorSelector).text(errorMessage).show();
-        return false;
-    } else {
-        $(errorSelector).hide();
-        return true;
-    }
 }
--- catshap/src/main/webapp/user_find_pw.jsp
+++ catshap/src/main/webapp/user_find_pw.jsp
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="./css/global.css" />
     <link rel="stylesheet" href="./css/main.css" />
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+	<script src="./js/user_regex.js"></script>
 	<script src="./js/user_find_pw.js"></script>
 </head>
 <body>
